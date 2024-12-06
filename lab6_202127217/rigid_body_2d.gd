4172eafa85560e5e6bad7e7e345fac1b9fde726b
LakkeTree <parkjoyjoy@ajou.ac.kr>
2024-12-06 23:44:18 +0900

    .
    .

--- lab6_202127217/rigid_body_2d.gd
+++ lab6_202127217/rigid_body_2d.gd
@@ -1,11 +1,19 @@
 extends RigidBody2D
 
+const SPEED = 300;
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var last_direction = Vector2.ZERO  
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:	
+	var input_direction = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	
+	if input_direction != Vector2.ZERO:		
+		last_direction = input_direction.normalized()
+	elif last_direction == Vector2.ZERO:		
+		linear_velocity = Vector2.ZERO
+		return
+	
+	linear_velocity = last_direction * SPEED
